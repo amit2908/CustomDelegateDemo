26030e274c1c9e0b2b9bd5b8fa209b20a9fdcb62
Shubham Ojha <ojhashubham29@gmail.com>
2017-10-02 19:55:42 +0530

    Custom Delegate Created

---
+++ CustomDelegateDemo/CustomDelegate.h
@@ -0,0 +1,13 @@
+//
+//  CustomDelegate.h
+//  CustomDelegateDemo
+//
+//  Created by Shubham Ojha on 10/2/17.
+//  Copyright © 2017 Shubham Ojha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol CustomDelegate <NSObject>
+
+@end
